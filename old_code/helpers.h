d93be6f73130df20500a88d4a22a327605dd7d5d
Sebastian Werhausen <swerhausen@googlemail.com>
2019-11-10 09:04:20 +0100

    Finally removed the old code

--- old_code/helpers.h
+++
@@ -1,8 +0,0 @@
-#ifndef ICING_HELPERS_H
-#define ICING_HELPERS_H
-
-std::vector<double> linspace(double x_min, double x_max, unsigned int N);
-std::vector<double> normalSpace(double x_min, double x_max, unsigned int N, double mu, double sigma);
-std::string to_string(double const & value);
-
-#endif
